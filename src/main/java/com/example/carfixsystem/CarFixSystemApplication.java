49d3763e542558a2b84f90876c3de2fe16eb52d5
zb <3174404699@qq.com>
2022-05-10 13:21:49 +0800

    second commit

--- src/main/java/com/example/carfixsystem/CarFixSystemApplication.java
+++ src/main/java/com/example/carfixsystem/CarFixSystemApplication.java
@@ -33,7 +33,7 @@ import java.util.Collections;
 public class CarFixSystemApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("helll");
         SpringApplication.run(CarFixSystemApplication.class, args);
         /*FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/carfixsystemdb?useUnicode=true&characterEncoding=utf-8", "root", "Aa135798")
                 .globalConfig(builder -> {
